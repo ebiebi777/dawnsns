a98badc1c78800eff857899a670a9f740c61df8f
ebi73 <okapippi7@yahoo.co.jp>
2021-11-13 21:52:16 +0900

    バリデーション・名前表示作成

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -61,6 +61,26 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\User
      */
+
+//バリデーション
+public function store(array $val)
+{
+return Validator::make($val, [ //makeでインスタンス？
+'username' => 'required|min:4|max:12',
+'mail' => 'required|min:4|max:50|unique:users',
+'password' => ['required', 'unique:users', 'regex:/^[0-9a-zA-Z]{4,12}+$/'],
+'password-confirm' => ['required', 'same:password', 'regex:/^[0-9a-zA-Z]{4,12}+$/'] //「数字の0～9」「英語のa～z」「英語のA～Z」「文字数4~12」
+], [
+'required' => '必須項目です',
+'min' => '4文字以上で入力してください',
+'max' => '12文字以内で入力してください',
+'unique' => 'すでに使われています',
+'same' => 'パスワードが異なります',
+'regex' =>'半角英数字4～12文字でお願いします'
+]);
+}
+
+
     protected function create(array $data)
     {
         return User::create([
@@ -70,20 +90,25 @@ class RegisterController extends Controller
         ]);
     }
 
-
     // public function registerForm(){
     //     return view("auth.register");
     // }
 
-    public function register(Request $request){
-        if($request->isMethod('post')){
-            $data = $request->input();
+    public function register(Request $request){ //新規登録
+if ($request->isMethod('post')) { //postできてるかどうか
+$data = $request->input(); //中身を$dataに格納
 
-            $this->create($data);
-            return redirect('added');
-        }
-        return view('auth.register');
-    }
+$validate = $this->store($data); //storeに飛ばす
+if ($validate->fails()) {
+return redirect('/register')->withErrors($validate)->withInput(); //バリデーションがエラーで戻った時に値を保持
+} else {
+$this->create($data); //createに処理を飛ばしている
+
+return redirect('added')->with('data', $data['username']); //addedにリダイレクトdateにusernameを渡す
+}
+}
+return view('auth.register');
+}
 
     public function added(){
         return view('auth.added');
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,22 +7,26 @@
 <h2>新規ユーザー登録</h2>
 
 {{ Form::label('ユーザー名') }}
-{{ Form::text('username',null,['class' => 'input']) }}
+{{ Form::text('username',null,['class' => 'input',]) }}
+@if($errors->has('username'))<p class="error">{{ $errors->first('username') }}</p> @endif
 
 {{ Form::label('メールアドレス') }}
-{{ Form::text('mail',null,['class' => 'input']) }}
+{{ Form::text('mail',null,['class' => 'input',]) }}
+@if($errors->has('mail'))<p class="error">{{ $errors->first('mail') }}</p> @endif
 
 {{ Form::label('パスワード') }}
 {{ Form::text('password',null,['class' => 'input']) }}
+@if($errors->has('password'))<p class="error">{{ $errors->first('password') }}</p> @endif
 
 {{ Form::label('パスワード確認') }}
 {{ Form::text('password-confirm',null,['class' => 'input']) }}
+@if($errors->has('password-confirm'))<p class="error">{{ $errors->first('password-confirm') }}</p> @endif
 
 {{ Form::submit('登録') }}
+
 
 <p><a href="/login">ログイン画面へ戻る</a></p>
 
 {!! Form::close() !!}
-
 
 @endsection
