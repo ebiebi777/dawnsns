e6fb22a03d5615123ec063f86f62682683ccd05b
ebi73 <okapippi7@yahoo.co.jp>
2021-11-21 22:22:53 +0900

    検索機能の追加

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Follow;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -9,5 +12,29 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+public function __construct(){
+
+// $this->middleware('auth');
+$this->middleware(function ($request, $next)
+{
+//ログインユーザー名
+$username = Auth::user(); //ログインユーザーの情報が$usernameに入る 全部入っちゃう
+//フォロワーのカウント
+$countFollower = Follow::where('follow', '=', Auth::id())
+->count();
+//フォローのカウント
+$countFollow = Follow::where('follower', '=', Auth::id())
+->count();
+
+//全ビューで共通で使えるよう渡してあげる。むっちゃ素敵。
+View::share('username', $username['username']); //view側に渡してあげる
+View::share('userimage', $username['images']);
+View::share('countFollower', $countFollower);
+View::share('countFollow', $countFollow);
+
+return $next($request);
+});
+}
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth; //auth使うならかく
 
 class UsersController extends Controller
 {
@@ -10,7 +11,23 @@ class UsersController extends Controller
     public function profile(){
         return view('users.profile');
     }
-    public function search(){
-        return view('users.search');
+
+    public function search(Request $request){
+        $list = \DB::table('users') //usersテーブルからもってきた
+        ->select('users.*', 'username', 'images') //usersの中からusernameとimagesだけもってくる
+        ->where('id','<>',Auth::id()) //自分のIDを消す
+        ->get(); //全部取得
+
+        if (!empty($request -> input('search'))) { //検索機能　$requestのinputのsearchがあれば処理が走る
+        $key = $request -> input('search'); //入力した内容をkeyへ
+        $list = \DB::table('users')
+        ->select('users.*')
+        ->where('username','like','%'. $key .'%') //あいまい検索
+        ->get(); //全部取得
+        return view('users.search',['list'=>$list,'key'=>$key]); //keyがあったらsearch.blade処理が走るので戻るボタンが出る
+        }
+        return view('users.search',['list'=>$list]); //何もない時の処理
     }
+
+
 }
