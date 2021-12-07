37931345609d9cb68d4ee6a5dc084642d86eacae
ebi73 <okapippi7@yahoo.co.jp>
2021-12-07 15:45:50 +0900

    ハンバーガーメニュー

--- public/js/script.js
+++ public/js/script.js
@@ -9,3 +9,18 @@ $(function () {
     });
   });
 });
+
+
+/*後で確認!!　ハンバーガーメニュー*/
+
+$(function () {
+  $('.menu-trigger').click(function () { //メニューボタンタップ後の処理
+    $(this).toggleClass('active'); //クリックした要素に「.active」要素を付与
+    $('.gnavi').css('display', 'block');//「.gnavi」要素の非表示を表示する
+    if ($(this).hasClass('active')) { //もしクリックした要素に「.active」要素があれば
+      $('.gnavi').addClass('active');　 //「.active」要素を付与
+    } else {                            //「.active」要素が無ければ
+      $('.gnavi').removeClass('active'); //「.active」要素を外す
+    }
+  });
+});
